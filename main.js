da32a25dd0e1110f28651079bfdfec59623d4f0e
Tom Aitken <tom@shop-grok.com>
2021-11-08 16:08:19 +1100

    correction

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const btn = $('#btn');
 const hideBtn = $('.hide-me');
 const copyBtn = $('.copy-me');
 
-const ArgumentParser = require('argparse');
+const { ArgumentParser } = require('argparse');
 const curlParser = new ArgumentParser({
     description: 'Basic arg parser for curl command'
 });
